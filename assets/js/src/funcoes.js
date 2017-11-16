25c9e0ced7baebd67d6d9af4be8a1dfe13ad083b
FelipeKusuki <felipesk26@gmail.com>
2017-11-16 18:18:14 -0200

    adicionando validacao de campos no cadastro

--- assets/js/src/funcoes.js
+++ assets/js/src/funcoes.js
@@ -1,5 +1,6 @@
+//Funcao que verifica se todos os campos do cadastra motorista estao preenchidos
 function checaMoto(){
-	if 	(document.Cadastro_moto.getElementById("textNome").value !=""
+	if 	(document.Cadastro_moto.textNome.value !=""
 		&& document.Cadastro_moto.Birthday.value !=""
 		&& document.Cadastro_moto.CPF.value !=""
 		&& document.Cadastro_moto.Modelo_carro.value !=""
@@ -7,6 +8,36 @@ function checaMoto(){
 		){
 		alert("Enviado");
 		document.Cadastro_moto.submit();
+
+	}else{
+		alert("Há campos não preenchidos");
+		 }
+
+ }
+
+//Funcao que verifica se todos os campos do cadastra corridas estao preenchidos
+ function checaCorr(){
+	if 	(document.Cadastro_corr.textNome.value !=""
+		&& document.Cadastro_corr.nome_pass.value !=""
+		&& document.Cadastro_corr.Valor.value !=""
+		){
+		alert("Enviado");
+		document.Cadastro_corr.submit();
+
+	}else{
+		alert("Há campos não preenchidos");
+		 }
+
+ }
+
+//Funcao que verifica se todos os campos do cadastra passageiro estao preenchidos
+ function checaPass(){
+	if 	(document.cadastro_pass.textNome.value !=""
+		&& document.cadastro_pass.Birthday.value !=""
+		&& document.cadastro_pass.CPF.value !="")
+	{
+		alert("Enviado");
+		document.cadastro_pass.submit();
 
 	}else{
 		alert("Há campos não preenchidos");
